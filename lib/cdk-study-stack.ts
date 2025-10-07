981608352ce4c6fdf0671881c55e97c783530607
andsaki <ruby.on.rails.api@gmail.com>
2025-10-07 23:57:53 +0900

    feat: Set up CDK project and add DynamoDB table

--- lib/cdk-study-stack.ts
+++ lib/cdk-study-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// DynamoDBモジュールをインポートします
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
+// cdk.Stackを継承したCdkStudyStackクラスを定義します。
+// このクラスがCloudFormationスタックに相当します。
 export class CdkStudyStack extends cdk.Stack {
+  // コンストラクタ: このスタックがインスタンス化されるときに呼び出されます。
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // ここに、このスタックを構成するAWSリソースを定義していきます。
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStudyQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // DynamoDBテーブルの定義
+    const table = new dynamodb.Table(this, 'TodoTable', {
+      // パーティションキー（主キー）の設定
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      // スタック削除時にテーブルも削除する設定 (学習用)
+      // 本番環境ではデータを保持するために cdk.RemovalPolicy.RETAIN を使用することが多いです。
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
   }
 }
--- test/cdk-study.test.ts
+++ test/cdk-study.test.ts
@@ -1,17 +1,25 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkStudy from '../lib/cdk-study-stack';
+// AWS CDKのコアライブラリをインポートします。
+import * as cdk from 'aws-cdk-lib';
+// アサーション（テストの検証）用のテンプレートライブラリをインポートします。
+import { Template } from 'aws-cdk-lib/assertions';
+// テスト対象のスタック（lib/cdk-study-stack.ts）をインポートします。
+import * as CdkStudy from '../lib/cdk-study-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-study-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkStudy.CdkStudyStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+// TODOアプリ用のテストをここに追加していきます。
+test('DynamoDB Table Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkStudy.CdkStudyStack(app, 'MyTestStack');
+  // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  // 生成されたテンプレートに、指定したパーティションキーを持つDynamoDBテーブルが存在するかを検証します。
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    KeySchema: [
+      {
+        AttributeName: 'id',
+        KeyType: 'HASH' // HASH = Partition key
+      }
+    ]
+  });
 });
